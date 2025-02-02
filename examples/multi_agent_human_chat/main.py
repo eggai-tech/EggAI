53584936940cdabd70e271c82f803283e637b7bc
Stefano Tucci <stefanotucci89@gmail.com>
2025-02-03 00:07:14 +0100

    feat: add OrderAgent with order handling functionality and set default transport to Kafka

--- examples/multi_agent_human_chat/main.py
+++ examples/multi_agent_human_chat/main.py
@@ -3,6 +3,7 @@ import asyncio
 import dspy
 from dotenv import load_dotenv
 from eggai.hooks import eggai_set_conf
+from eggai.transport import eggai_set_default_transport, KafkaTransport
 
 from billing_agent import billing_agent
 from escalation_agent import escalation_agent
@@ -20,8 +21,7 @@ async def main():
     language_model = dspy.LM("openai/gpt-4o-mini", cache=False)
     dspy.configure(lm=language_model)
 
-    # UNCOMMENT THIS IF YOU WANT TO RUN IT WITH KAFKA
-    # eggai_set_default_transport(lambda: KafkaTransport())
+    eggai_set_default_transport(lambda: KafkaTransport())
 
     await policies_agent.start()
     await escalation_agent.start()
