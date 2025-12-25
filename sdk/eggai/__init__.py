c65b1e4f1030f6b184ffedc0ce60d2a55be2af64
pontino <stefanotucci89@gmail.com>
2025-12-25 12:01:37 +0100

    Add __version__ and transport exports to main package

--- sdk/eggai/__init__.py
+++ sdk/eggai/__init__.py
@@ -1,3 +1,5 @@
+__version__ = "0.2.8"
+
 from .agent import Agent as Agent
 from .channel import Channel as Channel
 from .hooks import (
@@ -5,4 +7,9 @@ from .hooks import (
     eggai_register_stop as eggai_register_stop,
     eggai_main as eggai_main,
     EggaiRunner as EggaiRunner,
+)
+from .transport import (
+    KafkaTransport as KafkaTransport,
+    InMemoryTransport as InMemoryTransport,
+    RedisTransport as RedisTransport,
 )
